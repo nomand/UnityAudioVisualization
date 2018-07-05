a7f169d793bc93e1a4352817ea9204710f31d34d
nomand <alexey.botkov@gmail.com>
2018-07-05 16:34:20 +1200

    part 3

---
+++ Assets/Components/Scripts/AudioPeer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource audioSource;
+    public static float[] samples = new float[512];
+
+
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+    {
+        GetSpectrumAudioSource();
+	}
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ Assets/Components/Scripts/Instatiate512Cubes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instatiate512Cubes : MonoBehaviour {
+
+    public GameObject sampleCubePrefab;
+    public float radius = 100f;
+    public float scale = 2f;
+
+    private GameObject[] sampleCube = new GameObject[512];
+
+	void Start ()
+    {
+		for(int i = 0; i < 512; i++)
+        {
+            GameObject instance = (GameObject)Instantiate(sampleCubePrefab);
+            instance.transform.position = this.transform.position;
+            instance.transform.parent = this.transform;
+            instance.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i);
+            instance.transform.position = Vector3.forward * radius;
+            sampleCube[i] = instance;
+        }
+	}
+	
+	void Update ()
+    {
+		for(int i = 0; i < 512; i++)
+        {
+            if(sampleCube != null)
+            {
+                sampleCube[i].transform.localScale = new Vector3(10, AudioPeer.samples[i] * scale + 2, 10);
+            }
+        }
+                
+	}
+}
